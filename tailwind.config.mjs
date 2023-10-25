b6f17fdfcf422b24ce45f22ebaf3d9e5503c15e4
Torkil Adsersen <torkil.adsersen@gmail.com>
2023-10-25 12:15:26 +0200

    Added darkmode colors and link standards to project.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,8 @@
 
 const defaultTheme = require("tailwindcss/defaultTheme")
 
+const plugin = require("tailwindcss/plugin")
+
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -12,5 +14,24 @@ export default {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		plugin(function({addComponents}) {
+			addComponents({
+				'.link-light': {
+					textDecoration: 'underline',
+					color: '#fbbf24', /* AMBER-400 */
+					'&:hover': {
+						color: '#fcd34d' /* AMBER-300 */
+					},
+				},
+				'.link-dark': {
+					textDecoration: 'underline',
+					color: '#9a3412', /* ORANGE-800 */
+					'&:hover': {
+						color: '#c2410c' /* ORANGE-700 */
+					},
+				}
+			})
+		})
+	],
 }
