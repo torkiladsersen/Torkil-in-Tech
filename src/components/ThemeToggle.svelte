0e90a5f34ab8ac02f04637cd0e27594b9e3507d7
Torkil Adsersen <torkil.adsersen@gmail.com>
2023-11-14 10:19:12 +0100

    Change design on dark mode toggle

--- src/components/ThemeToggle.svelte
+++ src/components/ThemeToggle.svelte
@@ -15,19 +15,8 @@
 
 <button
   on:click={() => (darkScheme = !darkScheme)}
-  class="relative h-[22px] w-[42px] rounded-full bg-zinc-200 dark:bg-zinc-600"
->
-  <div
-    class="absolute -left-[0px] -top-[0px] h-[22px] w-[22px] rounded-full bg-sky-500 transition-all dark:translate-x-[20px] dark:bg-sky-900"
-  ></div>
-  <div
-    class="absolute left-[4px] top-[4px] w-[14px] text-yellow-300 dark:text-zinc-400"
-  >
-    <slot name="light" />
-  </div>
-  <div
-    class="absolute right-[4px] top-[4px] w-[13px] text-zinc-400 dark:text-sky-100"
-  >
-    <slot name="dark" />
-  </div>
+  class="relative flex items-stretch gap-2 px-2"
+  ><div class="dark:text-zinc-500">light</div>
+  <div class=" w-0.5 bg-black dark:bg-white"></div>
+  <div class="text-zinc-500 dark:text-white">dark</div>
 </button>
