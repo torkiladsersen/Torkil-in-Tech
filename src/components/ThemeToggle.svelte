6de67c0955679823f6c34d807e247c8d823ea151
Torkil Adsersen <torkil.adsersen@gmail.com>
2024-05-31 10:55:02 +0200

    Multiple changes to layout, color and components.

--- src/components/ThemeToggle.svelte
+++ src/components/ThemeToggle.svelte
@@ -15,8 +15,8 @@
 
 <button
   on:click={() => (darkScheme = !darkScheme)}
-  class="relative flex items-stretch gap-2 px-2"
-  ><div class="dark:text-zinc-500">light</div>
-  <div class=" w-0.5 bg-black dark:bg-white"></div>
-  <div class="text-zinc-500 dark:text-white">dark</div>
+  class="relative flex items-stretch gap-2 border-2 text-indigo-700 dark:text-lime-400 hover:-translate-y-px hover:text-indigo-600 dark:hover:text-lime-300 dark:hover:border-lime-300 hover:border-indigo-600 border-indigo-700 dark:border-lime-400 rounded-3xl py-1 px-2 shadow-indigo-400/20 hover:shadow-indigo-400/30 dark:shadow-lime-400/30 dark:hover:shadow-lime-400/40 shadow-lg transition-all duration-75"
+  >
+    <div class="iconify ph--sun-bold opacity-100 dark:opacity-50"></div>
+    <div class="iconify ph--moon-bold opacity-50 dark:opacity-100"></div>
 </button>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 const plugin = require("tailwindcss/plugin")
+const { addIconSelectors } = require('@iconify/tailwind');
 
 
 export default {
@@ -12,50 +13,27 @@ export default {
 		extend: {
 			fontFamily:{
 				sans: ["'Libre Franklin Variable'", ...defaultTheme.fontFamily.sans]
-			},
+			}
 		},
 	},
 	plugins: [
+		addIconSelectors(['ph']),
 		plugin(function({addComponents}) {
 			addComponents({
 				'.link-light': {
 					textDecoration: 'underline',
-					color: '#fbbf24', /* AMBER-400 */
+					color: '#a3e635', /* LIME-400 */
 					'&:hover': {
-						color: '#fcd34d' /* AMBER-300 */
+						color: '#bef264' /* LIME-300 */
 					},
 				},
 				'.link-dark': {
 					textDecoration: 'underline',
-					color: '#9a3412', /* ORANGE-800 */
+					color: '#4338ca', /* INDIGO-700 */
 					'&:hover': {
-						color: '#c2410c' /* ORANGE-700 */
+						color: '#4f46e5' /* INDIGO-600 */
 					},
 				},
-				'.bg-link-light': {
-					backgroundColor: '#fbbf24', /* AMBER-400 */
-					'&:hover': {
-						backgroundColor: '#fcd34d' /* AMBER-300 */
-					},
-				},
-				'.bg-link-dark': {
-					backgroundColor: '#9a3412', /* ORANGE-800 */
-					'&:hover': {
-						backgroundColor: '#c2410c' /* ORANGE-700 */
-					},
-				},
-				'.border-link-light': {
-					borderColor: '#fbbf24', /* AMBER-400 */
-					'&:hover': {
-						borderColor: '#fcd34d' /* AMBER-300 */
-					},
-				},
-				'.border-link-dark': {
-					borderColor: '#9a3412', /* ORANGE-800 */
-					'&:hover': {
-						borderColor: '#c2410c' /* ORANGE-700 */
-					},
-				}
 			})
 		})
 	],
